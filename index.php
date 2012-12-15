ae893f6977cc49571714c708121455ed1e215282
matfin <matfin@gmail.com>
2012-12-15 22:18:20 +0000

    Added html sections for CV

--- index.php
+++ index.php
@@ -2,11 +2,62 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Matt Finucane &ndash; Curriculum Vitae</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/stylesheets/screen.css" />
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <header>
+            <h1>
+                Matt Finucane &ndash; Curriculum Vitae
+            </h1>
+            <a href="mailto:matt@mattfinucane.ie" title="Email Matt Fimucane">
+                matt@mattfinucane.ie
+            </a>
+            <a href="tel:+353861945324" title="Phone Matt Finucane">
+                +353 (0)86 1945324
+            </a>
+            <span>
+                Referees available on request
+            </span>
+        </header>
+        <section id="profile">
+            <h2>
+                Profile
+            </h2>
+            <p>
+                Web and mobile application developer with over 7 years of professional industry experience. 
+            </p>
+        </section>
+        <section id="professional_experience">
+            <h2>
+                Professional Work Experience
+            </h2>
+            <h3>
+                Maith&uacute; IT Solutions
+            </h3>
+            <h4>
+                Dublin &ndash; Ireland. <time datetime="2012-01-16">January</time> to <time datetime="now">Present</time>
+            </h4>
+            <ol>
+                <li data-languages="" data-frameworks="">
+                    
+                </li> 
+            </ol> 
+        </section>
+        <section id="technical_skills">
+            <h2>
+                Technical Skills
+            </h2>
+        </section>
+        <section id="qualifications">
+            <h2>
+                Qualifications
+            </h2>
+        </section>
+        <section id="interests">
+            <h2>
+                Interests
+            </h2>
+        </section>
     </body>
 </html>
