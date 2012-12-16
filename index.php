6b5621284ab9bd6633930a548301b7fa2d5eeec9
matfin <matfin@gmail.com>
2012-12-16 02:34:01 +0000

    Basic HTML for CV put together

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Matt Finucane &ndash; Curriculum Vitae</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/stylesheets/screen.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/stylesheets/screen.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="assets/css/stylesheets/print.css" media="print" />
     </head>
     <body>
         <header>
@@ -32,32 +33,292 @@
             <h2>
                 Professional Work Experience
             </h2>
+            <!-- Company -->
             <h3>
                 Maith&uacute; IT Solutions
             </h3>
             <h4>
-                Dublin &ndash; Ireland. <time datetime="2012-01-16">January</time> to <time datetime="now">Present</time>
+                Dublin &ndash; Ireland. <time datetime="2012-01-16">January</time> to <time datetime="<?=date('Y-m-d')?>">Present</time>
             </h4>
-            <ol>
-                <li data-languages="" data-frameworks="">
-                    
-                </li> 
-            </ol> 
+            <!-- Project -->
+            <h5 data-languages="java, objective C, javascript, html5">
+                Irish Times iOS/Android app
+            </h5>
+            <p>
+                Worked with a team of three internal developers to build and deploy an <a href="#" title="View Irish Times on Apple App Store">iOS</a> and <a href="#" title="View Irish Times on Google Play Store">Android</a> version of the Irish Times app for phones. 
+            </p>
+            <p>
+                This app was completely re&ndash;designed and rebuilt from scratch natively in Objective C and Java. 
+            </p>
+             <!-- Project -->
+            <h5 data-languages="javascript, html5, java, objective c">
+                NewsWhip iOS/Android app
+            </h5>
+            <p>
+                This app was developed using Sencha Touch 2 as a cross platform solution and deployed to both Android and iOS devices. Due to performance issues with Sencha Touch 2 and Android, this app is also being written natively.
+            </p>
+             <!-- Project -->
+            <h5 data-languages="javascript, html5, java, objective c">
+                Galway University Hospital &ndash; Antimicrobial Guidelines 
+            </h5>
+            <p>
+                This app, which acts as a guide for doctors in diagnosing and treating infections is being coded using Sencha Touch 2 for iPhone and Android devices. 
+            </p>
+             <!-- Project -->
+            <h5 data-languages="javascript, html5, java, objective c">
+                3Football app
+            </h5>
+            <p>
+                This was the official new app for the Irish Soccer Team released during the Euro 2012 tournament. It featured news updates from the squad, match fixtures and results, and a fun stuff section with fan submitted data.
+            </p>
+            <p>
+                This app was coded using the Javascript based Sencha Touch 2 framework for cross platform compatibility and released for iPhone and Android devices.
+            </p>
+             <!-- Project -->
+            <h5 data-languages="c#, javascript, html5">
+                iBox TV
+            </h5>
+            <p>
+                This project was a web based interface for TV set top box software designed to run on HLS compatible devcies. In this case, the backend was coded in c# and run on Microsoft Servers. The front end was split into three submodules designed to run in Safari for the Mac, a customised Linux set top box with Opera installed, and GoogleTV boxes.
+            </p>
+             <!-- Company -->
+            <h3>
+                Paddy Power PLC
+            </h3>
+            <h4>
+                Dublin &ndash; Ireland. <time datetime="2010-03-08">March 2010</time> to <time datetime="2012-01-16">January 2012</time>
+            </h4>
+            <!-- Project -->
+            <h5 data-languages="php, javascript, html5">
+                Power Payroll System
+            </h5>
+            <p>
+                Worked with members of the Finance and HR team in developing a payroll web application for deployment to Paddy Power shops across the country.
+            </p>
+            <!-- Project -->
+            <h5 data-languages="php, javascript, html5">
+                Paddy Power Casino
+            </h5>
+            <p>
+                Developed custom CMS and front end from scratch for Paddy Power Casino, one of Paddy Power&#39;s many business channels.
+            </p>
+            <!-- Project -->
+            <h5 data-languages="php, javascript, html5">
+                Paddy Power Poker
+            </h5>
+            <p>
+                Extended Paddy Power Casino CMS to include a landing page for the Irish Winter Festival Poker Tournament in 2011
+            </p>
+            <!-- Project -->
+            <h5 data-languages="php, javascript, html5">
+                Paddy Power Games
+            </h5>
+            <p>
+                Worked on the CMS and frontend for the redesign and redevelopment from scratch for Paddy Power Games.
+            </p>
+            <!-- Project -->
+            <h5 data-languages="php, javascript, html5">
+                Paddy Power Mobile Games
+            </h5>
+            <p>
+                Redevloped and extended the PaddyPower mobile games cms to include data for multi&ndash;platform mobile devices.
+            </p>
         </section>
         <section id="technical_skills">
             <h2>
                 Technical Skills
             </h2>
+            <dl>
+                <dt>
+                    Programming languages
+                </dt>
+                <dd>
+                    Java
+                </dd>
+                <dd>
+                    PHP
+                </dd>
+                <dd>
+                    Objective C
+                </dd>
+                <dd>
+                    Javascript
+                </dd>
+                <dd>
+                    HTML5
+                </dd>
+                <dd>
+                    C#
+                </dd>
+                <dd>
+                    C++
+                </dd>
+            </dl>
+            <dl>
+                <dt>
+                    Frameworks
+                </dt>
+                <dd>
+                    Android
+                </dd>
+                <dd>
+                    Cocoa UIKit (iOS)
+                </dd>
+                <dd>
+                    Sencha Touch 2
+                </dd>
+                <dd>
+                    PhoneGap
+                </dd>
+                <dd>
+                    Compass SASS (CSS)
+                </dd>
+                <dd>
+                    Razr
+                </dd>
+                <dd>
+                    C# MVC3
+                </dd>
+                <dd>
+                    jQuery
+                </dd>
+                <dd>
+                    CodeIgniter PHP
+                </dd>
+            </dl>
+            <dl>
+                <dt>
+                    Version Control
+                </dt>
+                <dd>
+                    Git
+                </dd>
+                <dd>
+                    Gitosis
+                </dd>
+                <dd>
+                    Mercurial
+                </dd>
+                <dd>
+                    SVN
+                </dd>
+                <dd>
+                    SVK
+                </dd>
+                <dd>
+                    CVS
+                </dd>
+            </dl>
+            <dl>
+                <dt>
+                    Project management
+                </dt>
+                <dd>
+                    Jira
+                </dd>
+                <dd>
+                    Redmine
+                </dd>
+                <dd>
+                    Assembla
+                </dd>
+                <dd>
+                    BaseCamp
+                </dd>
+                <dd>
+                    Google Docs
+                </dd>
+            </dl>
+            <dl>
+                <dt>
+                    IDEs
+                </dt>
+                <dd>
+                    Xcode
+                </dd>
+                <dd>
+                    Eclipse
+                </dd>
+                <dd>
+                    Netbeans
+                </dd>
+                <dd>
+                    Visual Studio 2010
+                </dd>
+            </dl>
+            <dl>
+                <dt>
+                    Software dependency management
+                </dt>
+                <dd>
+                    CocoaPods
+                </dd>
+                <dd>
+                    RubyGems
+                </dd>
+            </dl>
+            <dl>
+                <dt>
+                    Databases &amp; Infrastructure
+                </dt>
+                <dd>
+                    Unix/Linux 
+                </dd>
+                <dd>
+                    OSX
+                </dd>
+                <dd>
+                    Apache
+                </dd>
+                <dd>
+                    Microsoft IIS
+                </dd>
+                <dd>
+                    Microsoft SQL Server 
+                </dd>
+                <dd>
+                    Linq
+                </dd>
+                <dd>
+                    MySQL
+                </dd>
+            </dl>
         </section>
         <section id="qualifications">
             <h2>
                 Qualifications
             </h2>
+            <h3>
+                University
+            </h3>
+            <h4>
+                Dublin City University, Dublin &ndash; Ireland. <time datetime="2001-09-26">September 2001</time> to <time datetime="2005-06-10">June 2005</time>
+            </h4>
+            <p>
+                BSc (Hons) in Computer Applications (Information Systems) and a six month work placement in Alcatel CIT, France.
+            </p>
+            <p>
+                My final year project was a 2D scrolling platform video game for mobile phones, developed using J2ME and run on entry level Nokia phones.
+            </p>
+            <ol>
+                <li>
+                    <a href="http://www.mattfinucane.ie/docs/Ero-Technical-Manual.pdf" title="Project Technical Manual">Technical Manual</a>
+                </li>
+                <li>
+                    <a href="http://www.mattfinucane.ie/docs/Ero-User-Manual.pdf" title="Project User Manual">User Manual</a>
+                </li>
+                <li>
+                    <a href="http://www.mattfinucane.ie/docs/animation_example.java" title="Project Source Manual">Source code</a>
+                </li>
+            </ol>
         </section>
         <section id="interests">
             <h2>
                 Interests
             </h2>
+            <p>
+                My main interests are reading, photography, film, web development and technology. I have worked on a pro bono basis developing websites for up and coming bands and music festivals. I am also combining my passion for photography and web development and working on my own website in HTML5 and CSS3 to showcase my images. I am also an avid reader of technology and web related blogs such as engadget.com, contrast.ie and slashdot.org.
+            </p>
         </section>
     </body>
 </html>
