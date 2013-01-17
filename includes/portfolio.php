35b826df58a68fa82546cb40b6d0c0848db0751a
matfin <matfin@gmail.com>
2013-01-17 21:52:14 +0000

    Portfolio content updated

--- includes/portfolio.php
+++ includes/portfolio.php
@@ -4,19 +4,16 @@
          *  Mobile work
          */
         new portfolioitem(
-            'The Irish Times', 
+            'The Irish Times iOS/Android', 
             'http://tinyurl.com/bfg42q9',
             'mobile/irishtimes.jpg', 
             'The Irish Times - a major broadsheet newspaper in Ireland, gave us the task of overhauling their app for iPhone and Android platforms.',
             'As one of the lead developers on this project, I was responsible for smoothly integrating existing news infrastructure into a fast, feature rich and robust app.',
             'mobile',
             array(
-                'As development lead I liased with other developers in planning app arhcitecture.',
-                'Worked with client to ensure smooth integration of existing technologies into the app.',
-                'Planned and developed major parts of the app in Objective C, such as the offline storage feature.',
-                'Used a web based beta release platform with integrate crash reporting tools.',
-                'Released periodic updates to the app for internal testing and bug fixing.',
-                'Used CocoaPods to easily manage third party libraries and dependencies.'
+                'Developed natively in Objective C and Java.',
+                'Downloads and caches content concurrently to ensure smooth user interface.',
+                'Used a web based beta testing and crash reporting tool to pinpoint problems.'
             )
         ),
         new portfolioitem(
@@ -27,11 +24,9 @@
             'Using their existing feed infrastructure, I was able to create a cross platform app using html5 and javascript.',
             'mobile',
             array(
-                'Worked as lead developer on this project and provided support to the front end designers.',
-                'Used a cross platform Javascript MVC based framework along with html5 and Compass SCSS',
-                'PhoneGap was used to leverage some native functionality vanilla Javascript cannot provide',
-                'Deployed test versions of the app regularly using the BetaBuilder deployment tool.',
-                'Released the app on the iTunes app store using iTunes Connect.'
+                'Developed cross platform for easier portability to other devices.',
+                'Makes use of most existing infrastructure to minimise back end development effort.',
+                'Content cached to minimise impact on server infrastructure and device bandwidth.'
             )
         ),
         new portfolioitem(
@@ -42,22 +37,9 @@
             'We set about creating an app that would run on even the most simple Android based devices.',
             'mobile',
             array(
-                'As lead developer I planned the app architecture with the rest of the team.',
-                'The app was developed using a cross platform MVC framework.',
-                'Communicated with Three to ensure smooth delivery of live match commentary data.',
-                'Optimised the app so it would work on lower powered iOS and Android devices.',
-                'Launched the app in the Summer of 2012'
-            )
-        ),
-        new portfolioitem(
-            'Photography', 
-            false,
-            'mobile/photography.jpg', 
-            'Native app pulling my photos from flickr',
-            'solution',
-            'mobile',
-            array(
-                
+                'App downloads and caches fixtures, match results live match commentary.',
+                'For maximum reach this app is optimised to run on low powered devices.',
+                'Content is populated using a widely used content management system.'
             )
         ),
         
@@ -65,91 +47,41 @@
          *  Web work
          */
         new portfolioitem(
-            'Angels Beacons of Hope', 
-            false,
-            'web/angels-beacons-of-hope.jpg', 
-            'This site was created pro&ndash;bono as part of a collaborative team effort in late 2009.',
-            'solution',
-            'web',
-            array(
-                
-            )
-        ),
-        new portfolioitem(
             'Dress For Success Dublin', 
             'http://www.dressforsuccessdublin.org',
             'web/dress-for-success-dublin.jpg', 
-            'One of the entries for 24 The Web, a 24 hour design challenge for charities. Site uses Perch as a cms and CodeIgniter for storing and retrieving contact information.',
-            'solution',
+            'This site for an international charity was one of three developed by a team of seven people as part of the 24 The Web design challenge.',
+            'I worked as lead developer within a team of six other people. My responsibility was to set up the server and content management infrastructure and integrate the final design.',
             'web',
             array(
-                
+                'Site comes integrated with Perch, a lightweight content management system.',
+                'CRM allows site owners to easily access volunteers information.'
             )
         ),
         new portfolioitem(
             'iBox streaming TV', 
             'http://www.ibox.ie',
             'web/ibox.jpg', 
-            'Web based TV guide interface for a subscription based TV streaming service in Ireland.',
-            'solution',
+            'iBox are a new internet based subscription TV streaming service launched in 2012 in Ireland. My task was to develop their TV guide interface for deployment across multiple devices.',
+            'I designed and created the server infrastructure needed to load channel and video data. I also created the front end TV guide interface.',
             'web',
             array(
-                
-            )
-        ),
-        new portfolioitem(
-            'LittleGreen',
-            false,
-            'web/littlegreen.jpg', 
-            'Wordpress based site for managing content. Integrated Facebook Connect for social sharing and Google Analytics for tracking.',
-            'solution',
-            'web',
-            array(
-                
+                'High performing web based interface optimised to work on devices with limited capacity.',
+                'Fresh TV channel data downloads in the background and is unobtrusive to the user.',
+                'Interface created using jQuery and html5 for easy portability to new TV technologies.'
             )
         ),
         new portfolioitem(
             'PaddyPower Casino', 
             'http://casino.paddypower.com',
             'web/paddypower-casino.jpg', 
-            'Created with custom built CMS using CodeIgniter for front end and back end.',
-            'solution',
+            'An overhaul was needed for the The Paddy Power Casino website to deliver a wider selection of games and customised content to the customer.',
+            'The site was redeveloped and redesigned with a custom built CMS and access to a wider variety of games.',
             'web',
             array(
-                
-            )
-        ),
-        new portfolioitem(
-            'PaddyPower Games', 
-            'http://games.paddypower.com',
-            'web/paddypower-games.jpg', 
-            'Redesigned and integrated custom cms using CodeIgniter framework.',
-            'solution',
-            'web',    
-            array(
-                
-            )
-        ),
-        new portfolioitem(
-            'Rhendy Chartered Accountants', 
-            'http://www.rhendy.ie',
-            'web/rhendy.jpg', 
-            'Content managed with Perch CMS and uses Facebook Connect and Twitter API for social sharing.',
-            'solution',
-            'web',
-            array(
-                
-            )
-        ),
-        new portfolioitem(
-            'Vantastival', 
-            'http://www.vantastival.com',
-            'web/vantastival.jpg', 
-            'Content managed using wordpress with integrated Facebook and Twitter social plugins for sharing content.',
-            'solution',
-            'web',
-            array(
-                
+                'Optimised delivery of images, CSS and Javascript to minimise load on servers.',
+                'CMS developed to allow content owners to deliver custom content to customer groups.',
+                'Smooth and secure integration of website with existing infrastructure.'
             )
         )
     );
