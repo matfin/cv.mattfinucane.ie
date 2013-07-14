6233bffb4a274e5097ca36e7577a5f94ece7c21d
matfin <matfin@gmail.com>
2013-07-14 22:23:41 +0100

    Updated current profile.

--- includes/profile.php
+++ includes/profile.php
@@ -2,5 +2,5 @@
     Profile
 </h2>
 <p>
-    Skilled web and application developer with over 8 years commercial experience. My passion is the application of modern technologies to solve real world problems, and I produce robust solutions of excellent quality. 
+    Skilled web and application developer with 8 years commercial experience. My passion is the application of modern technologies to solve real world problems, and I produce robust solutions of excellent quality. 
 </p>
