a6f4749bd16845a8f0782d8a2bd570958d26200d
Matt Finucane <matfin@gmail.com>
2014-04-15 00:03:21 +0200

    Tidied up static content and skills.

--- client/controllers/components/skills.js
+++ client/controllers/components/skills.js
@@ -10,5 +10,9 @@ Template['components_skills'].destroyed = function() {
 };
 
 Template['components_skills'].data = function() {
+	return App.models.staticContent.findOne({'title':'skills'});
+};
+
+Template['components_skills'].skillsets = function() {
 	return App.models.skills.find({}).fetch();
 };
--- packages/app/app.js
+++ packages/app/app.js
@@ -16,7 +16,7 @@ App = {
 
 	models: {
 		staticContent: new Meteor.Collection('staticContent', {connection: null}),
-		skills: new Meteor.Collection('skills', {connection: null}),
+		skills: new Meteor.Collection('skillsets', {connection: null}),
 		experience: new Meteor.Collection('experience', {connection: null}),
 		portfolio: new Meteor.Collection('portfolio', {connection: null})
 	}
