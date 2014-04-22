e88497e9d32f2c424fe47907c7e077708bd31ed6
Matt Finucane <matfin@gmail.com>
2014-04-22 11:20:22 +0200

    Created views for different sections of the site
    Added new views and updated routes.js to point to the correct content.
    Updated meteor version. CSS fixes.

--- routes.js
+++ routes.js
@@ -4,6 +4,27 @@ Router.map(function() {
 		template: 'main',
 		yieldTemplates: {
 			'components_common_header': {to: 'header'},
+			'views_home': {to: 'content'},
+			'components_common_footer': {to: 'footer'}
+		}
+	});
+
+	this.route('portfolio', {
+		path: '/portfolio',
+		template: 'main',
+		yieldTemplates: {
+			'components_common_header': {to: 'header'},
+			'views_portfolio': {to: 'content'},
+			'components_common_footer': {to: 'footer'}
+		}
+	});
+
+	this.route('about', {
+		path: '/about',
+		template: 'main',
+		yieldTemplates: {
+			'components_common_header': {to: 'header'},
+			'views_about': {to: 'content'},
 			'components_common_footer': {to: 'footer'}
 		}
 	});
